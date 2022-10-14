b57b45821af9b91c7bb81322ecaced3dfba4ec4d
Ryan Henderson <wrhenders@gmail.com>
2022-10-14 10:06:26 -0500

    added convert and stateful text field

--- app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
@@ -7,8 +7,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,18 +31,52 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainActivityContent(){
-    Header(R.drawable.sunrise, "sunrise image")
+    val celsius = remember { mutableStateOf(0)}
+    val newCelsius = remember { mutableStateOf("")}
+    Column() {
+        Header(R.drawable.sunrise, "sunrise image")
+        EnterTemperature(temperature = newCelsius.value, changed = {newCelsius.value = it})
+        ConvertButton {
+            newCelsius.value.toIntOrNull()?.let {celsius.value = it}
+        }
+        TemperatureText(celsius.value)
+    }
+    
 }
 
 @Composable
 fun Header(image: Int, description: String) {
     Image(
-        painter = painterResource(image),
+        painter = painterResource(image), 
         contentDescription = description,
-        modifier = Modifier.height(180.dp).fillMaxWidth(),
+        modifier = Modifier
+            .height(180.dp)
+            .fillMaxWidth(),
         contentScale = ContentScale.Crop)
 }
 
+@Composable
+fun TemperatureText(celsius: Int) {
+    val fahr = (celsius.toDouble()*9/5)+32
+    Text("$celsius Celsius is $fahr Fahrenheit")
+}
+
+@Composable
+fun EnterTemperature(temperature: String, changed: (String) -> Unit) {
+    TextField(
+        value = temperature,
+        label = { Text("Enter a temperature in Celsius")},
+        onValueChange = changed,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun ConvertButton(clicked: () -> Unit) {
+    Button(onClick = clicked) {
+        Text("Convert")
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
